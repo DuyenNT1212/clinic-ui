7749d4ac1186e43daba968901270fb332f95c285
DuyenNT1212 <nguyenduyen050726@gmail.com>
2021-12-11 08:57:33 +0700

    Init project and add Dockerfile.

--- src/component/MainHeader/index.jsx
+++ src/component/MainHeader/index.jsx
@@ -9,6 +9,9 @@ const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 export const MainHeader = () => {
+
+    const path = window.location.pathname;
+
     const menu = (
         <Menu>
           <Menu.Item key="0">
@@ -38,15 +41,15 @@ export const MainHeader = () => {
             </Col>
 
             <Col md={20}>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1"><Link to="/trangchu">Trang chủ</Link></Menu.Item>
-                <Menu.Item key="2"><Link to="/gioithieu">Giới thiệu</Link></Menu.Item>
-                <Menu.Item key="3"><Link to="/dichvutuvan">Dịch vụ - Tư vấn</Link></Menu.Item>
-                <Menu.Item key="4"><Link to="/hosodientu">Hồ sơ điện tử</Link></Menu.Item>
-                <Menu.Item key="5"><Link to="/nhakhoa">Nha khoa</Link></Menu.Item>
-                <Menu.Item key="6"><Link to="/doingubacsi">Đội ngũ bác sĩ</Link></Menu.Item>
-                <Menu.Item key="7"><Link to="/banggia">Bảng giá</Link></Menu.Item>
-                <Menu.Item key="8"><Link to="/datlichkham">Đặt lịch khám</Link></Menu.Item>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={path}>
+                <Menu.Item key="/trangchu"><Link to="/trangchu">Trang chủ</Link></Menu.Item>
+                <Menu.Item key="gioithieu"><Link to="/gioithieu">Giới thiệu</Link></Menu.Item>
+                <Menu.Item key="/dichvutuvan"><Link to="/dichvutuvan">Dịch vụ - Tư vấn</Link></Menu.Item>
+                <Menu.Item key="/hosodientu"><Link to="/hosodientu">Hồ sơ điện tử</Link></Menu.Item>
+                <Menu.Item key="/nhakhoa"><Link to="/nhakhoa">Nha khoa</Link></Menu.Item>
+                <Menu.Item key="/doingubacsi"><Link to="/doingubacsi">Đội ngũ bác sĩ</Link></Menu.Item>
+                <Menu.Item key="/banggia"><Link to="/banggia">Bảng giá</Link></Menu.Item>
+                <Menu.Item key="/datlichkham"><Link to="/datlichkham">Đặt lịch khám</Link></Menu.Item>
                 {/* <Dropdown overlay={menu} trigger={['click']}>
                     Quản trị <DownOutlined />
                 </Dropdown> */}
@@ -55,8 +58,8 @@ export const MainHeader = () => {
                 </Menu.Item> */}
                 {/* <Menu.Item key="9"> */}
                     <SubMenu key="9" title='Quản trị'>
-                        <Menu.Item key="10"><Link to="/danhsachbacsi">Danh sách bác sĩ</Link></Menu.Item>
-                        <Menu.Item key="11"><Link to="/danhsachbenh">Danh sách bệnh</Link></Menu.Item>
+                        <Menu.Item key="/danhsachbacsi"><Link to="/danhsachbacsi">Danh sách bác sĩ</Link></Menu.Item>
+                        <Menu.Item key="/danhsachbenh"><Link to="/danhsachbenh">Danh sách bệnh</Link></Menu.Item>
                     </SubMenu>
                 {/* </Menu.Item> */}
             </Menu>
